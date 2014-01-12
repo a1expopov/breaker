be92afc0ad28c27e574879d638b5e77c1f60f5c0
Alex <alexanderspopov@gmail.com>
2014-01-12 12:31:08 -0500

    Improving regex a bit and writing scrape result to text file

--- scrape/scrape.py
+++ scrape/scrape.py
@@ -5,5 +5,7 @@ import urllib2
 url = urllib2.urlopen('http://www.thecreativeham.com/agencies/new-york.php')
 doc = url.read()
 
-domains = re.findall('<td><a href="([^"]+)"[^>]+>', doc)
-print domains
+domains = [domain for www, domain in re.findall('<td><a href="http://(www\.)?(.+?)/?"[^>]+>', doc)]
+
+with open('scraped-domains.txt', 'wb') as fp:
+  fp.write('\n'.join(domains))
